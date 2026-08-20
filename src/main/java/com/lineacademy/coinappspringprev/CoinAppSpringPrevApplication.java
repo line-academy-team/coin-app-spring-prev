346d243678d41382e27e12da58fa8d7017f41db3
Tae.Garam (Office) <drasla217@gmail.com>
2026-08-20 10:01:59 +0900

    초기화

---
+++ src/main/java/com/lineacademy/coinappspringprev/CoinAppSpringPrevApplication.java
@@ -0,0 +1,13 @@
+package com.lineacademy.coinappspringprev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinAppSpringPrevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinAppSpringPrevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lineacademy/coinappspringprev/CoinAppSpringPrevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lineacademy.coinappspringprev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinAppSpringPrevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
